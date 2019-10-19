4fffeb2de5a36583013f95505b1858800d15de2e
Anchen <acmhuang@gmail.com>
2019-10-19 14:48:19 -0400

    implement useinfinitescroll with useeffect for 30 article load and autoload every 30

--- src/components/StoriesList.js
+++ src/components/StoriesList.js
@@ -1,14 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { getStoriesId } from "../services/hackerNewsApi";
 import StoriesListItem from "./StoriesListItem";
+import { useInfiniteScroll } from "../hooks/useInfiniteScroll";
 
 const StoriesList = () => {
+  const { count } = useInfiniteScroll();
   const [newStories, setStories] = useState([]);
 
   useEffect(() => {
     getStoriesId().then(data => setStories(data));
   }, []);
 
-  return newStories.map(storyId => <StoriesListItem key={storyId} storyId={storyId} />);
+  return (
+    <div>
+      {newStories.slice(0, count).map(storyIds => (
+        <StoriesListItem key={storyIds} storyId={storyIds} />
+      ))}
+    </div>
+  );
 };
 export default StoriesList;

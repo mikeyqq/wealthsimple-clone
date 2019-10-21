80fb66bb0aace4a10713a3062592fbc0eeefe8f8
Anchen <acmhuang@gmail.com>
2019-10-21 18:18:22 -0400

    refactor useinfinitescroll file

--- client/src/hooks/useInfiniteScroll.js
+++ client/src/hooks/useInfiniteScroll.js
@@ -7,21 +7,14 @@ export const useInfiniteScroll = () => {
   const [count, setCount] = useState(STORY_INCREMENT);
 
   const handleScroll = debounce(() => {
-    if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight || loading) {
-      return false;
-    } else {
-      setLoading(true);
-    }
+    window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight || loading
+      ? setLoading(false)
+      : setLoading(true);
   }, 500);
 
   useEffect(() => {
     if (!loading) return;
-
-    if (count + STORY_INCREMENT >= MAX_STORIES) {
-      setCount(MAX_STORIES);
-    } else {
-      setCount(count + STORY_INCREMENT);
-    }
+    count + STORY_INCREMENT >= MAX_STORIES ? setCount(MAX_STORIES) : setCount(count + STORY_INCREMENT);
     setLoading(false);
   }, [loading]);
 
